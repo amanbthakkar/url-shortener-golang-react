e8df2833c60567f606efb32bf591122beba16fb7
Aman Thakkar <aman.thakkar@somaiya.edu>
2023-12-02 20:05:23 -0800

    fixed redirection and failure to redirect

--- frontend_test/url-shortener-website/src/App.js
+++ frontend_test/url-shortener-website/src/App.js
@@ -1,3 +1,5 @@
+// App.js
+
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
@@ -10,8 +12,6 @@ function App() {
   useEffect(() => {
     // Extract the shortened part from the URL
     const shortenedPart = window.location.pathname.substring(1);
-    console.log(window.location.pathname);
-    console.log('Shortened path:', shortenedPart);
     // If there is a shortened part, make the GET request
     if (shortenedPart) {
       handleRedirect(shortenedPart);
@@ -21,19 +21,22 @@ function App() {
   const handleRedirect = async (shortenedPart) => {
     try {
       const newURL = 'http://localhost:5000?shortened=' + shortenedPart;
-      console.log(newURL);
       const response = await axios.get(newURL);
-
       if (response.status === 200) {
         const originalURL = response.data.originalURL;
         window.location.href = originalURL;
-      } else {
-        console.log('sadas');
-        window.location.replace('/');
       }
     } catch (error) {
+      window.location.href = 'http://localhost:3000';
+
       console.error('Error handling redirect:', error.message);
     }
+  };
+
+  const handleCopy = () => {
+    // Copy the shortened URL to the clipboard
+    navigator.clipboard.writeText(`http://localhost:3000/${outputURL}`);
+    alert('Shortened URL copied to clipboard!');
   };
 
   const handleSubmit = async () => {
@@ -42,7 +45,6 @@ function App() {
     };
     try {
       const response = await axios.post('http://localhost:5000', submitObject);
-      console.log(response.data);
       setOutputURL(response.data.value);
     } catch (error) {
       console.error('Error submitting data:', error.message);
@@ -50,21 +52,34 @@ function App() {
   };
 
   return (
-    <div>
-      <label htmlFor='longURL'></label>
-      <input
-        id='longURL'
-        value={inputURL}
-        onChange={(e) => setInputURL(e.target.value)}
-      ></input>
-      <button onClick={handleSubmit}>Shorten this URL!</button>
+    <div className='container'>
+      <div className='input-container'>
+        <input
+          id='longURL'
+          className='input-field'
+          placeholder='Enter a URL to be shortened'
+          value={inputURL}
+          onChange={(e) => setInputURL(e.target.value)}
+        />
+      </div>
+
+      <button className='submit-button' onClick={handleSubmit}>
+        Shorten this URL!
+      </button>
+
       {outputURL && (
-        <p>
-          Shortened URL:{' '}
-          <a href={`http://localhost:3000/${outputURL}`}>
-            amanthakkar.com/{outputURL}
-          </a>
-        </p>
+        <div className='output-url'>
+          <p>
+            "{inputURL}" is shortened to "
+            <a href={`http://localhost:3000/${outputURL}`}>
+              amanthakkar.com/{outputURL}
+            </a>
+            "{' '}
+            <span role='img' aria-label='copy-icon' onClick={handleCopy}>
+              📋
+            </span>
+          </p>
+        </div>
       )}
     </div>
   );
